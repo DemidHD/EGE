c0cd69b51d73bb70a80f67bb320edac918909882
Demid <demid.trofimov.2017@mail.ru>
2023-03-20 19:32:49 +0300

    Изменена файловая структура

--- 2_ex.py
+++
@@ -1,8 +0,0 @@
-print('x y z w')
-"""2"""
-for x in range(0, 2):
-    for y in range(0, 2):
-        for z in range(0, 2):
-            for w in range(0, 2):
-                if ((x and y) or (y and z)) == ((x <= w) and (w <= z)):
-                    print(x, y, z, w)
