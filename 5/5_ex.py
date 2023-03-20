d6cbb6feb418374e9a3be1128f0bbeab82e423a9
Demid <demid.trofimov.2017@mail.ru>
2023-03-20 19:58:49 +0300

    Добавлены примеры

--- 5/5_ex.py
+++ 5/5_ex.py
@@ -1,11 +1,12 @@
 for i in range(1, 1000):
-    """5"""
     bin_x = bin(i)[2:]
     if i % 2 == 0:
         bin_x = bin_x + '01'
     else:
         bin_x = '1' + bin_x + '10'
 
-    if int(bin_x, 2) > 214:
+    base_10 = int(bin_x, 2)
+
+    if base_10 > 214:
         print(i)
         break
